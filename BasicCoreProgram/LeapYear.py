c1680fefdf1b32a77967c2e9653989738e0a63cb
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-30 15:48:01 +0530

    [Shruti]Add: To check year is leap year or not

---
+++ BasicCoreProgram/LeapYear.py
@@ -0,0 +1,19 @@
+'''
+@Author: Shruti
+@Date: 2021-12-30
+@Last Modified by: shruti
+@Last Modified time: 2021-12-30 
+@Title : To check whether entered year is a leap year or not
+'''
+
+year=int(input("Enter the year: "))
+if year % 4==0:
+    if year % 100==0:
+        if year % 400==0:
+            print(year,"is a leap year")
+        else:
+            print(year,"is not a leap year")
+    else:
+        print(year,"is a leap year")
+else:
+    print(year,"Not a leap year")            
