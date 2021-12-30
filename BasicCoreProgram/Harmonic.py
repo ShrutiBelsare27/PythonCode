2b03e780b26db8a83ae8345a8e45266149d9985b
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-31 00:42:04 +0530

    [Shruti]Added: Print Harmonic number and sum of value

---
+++ BasicCoreProgram/Harmonic.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shruti
+@Date: 2021-12-30
+@Last Modified by: Shruti
+@Last Modified time: 2021-12-30 
+@Title : To check whether entered year is a leap year or not
+'''
+
+sum=0
+n=int(input("Enter a number "))
+if n!=0:
+    i=1
+    for i in range(1,n+1):
+        sum=sum+(1/i) # harmonic sum calculation
+        print("value of 1/",i," is ",  1/i)
+    print("Harmonic sum is",sum)
+else:
+    print("Number should not equal to zero")
