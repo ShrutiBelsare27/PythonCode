e734193779024c1e832f91bd827f273170adcb55
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-12-30 20:54:33 +0530

    [Shruti]Added: Replace string template

---
+++ BasicCoreProgram/StringReplace.py
@@ -0,0 +1,17 @@
+'''
+@Author: Shruti
+@Date: 2021-12-30
+@Last Modified by: Shruti
+@Last Modified time: 2021-12-30 
+@Title : 
+'''
+
+ 
+userName=input("Please enter your name :") 
+if userName.isalpha(): 
+    if len(userName)>3: 
+        print("Hello",userName," How are you ?")
+    else:
+        print("Please enter more than 3 characters")
+else: 
+    print("Not a valid input string")
